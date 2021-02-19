37964751985850540168deeacba12e4ddec28d26
Fernando Valladares Monteiro <fvallada@usc.edu>
2021-02-18 16:45:14 -0800

    Refactors code to allow reading and working with either VISSIM or NGSIM data easily

--- SSMEstimation/main.py
+++ SSMEstimation/main.py
@@ -35,14 +35,32 @@ def run_i170_scenario(save_veh_record=False):
         vi.run_i710_simulation(sim_params, idx_scenario, demand, save_veh_record)
 
 
-def post_process_and_save(network_file):
-    data_analyzer = DataAnalyzer(VissimInterface.networks_folder, network_file, raw=True)
-    data_analyzer.post_process_vissim_data()
-    data_analyzer.save_to_csv(VissimInterface.networks_folder)
+def post_process_and_save(data_source, network_name):
+    if data_source.lower() == 'vissim':
+        data_reader = readers.VissimDataReader(network_name)
+    elif data_source.lower() == 'ngsim':
+        data_reader = readers.NGSIMDataReader(network_name)
+    else:
+        print('Trying to process data from unknown data source')
+        return
+
+    print('Loading data from {}, network: {}'.format(data_source, network_name))
+    data = data_reader.load_data()
+    data_analyzer = DataAnalyzer(data_source, data)
+    data_analyzer.post_process_data()
+    data_analyzer.save_to_csv(network_name)
 
 
-def create_all_ssm(network_file):
-    data_analyzer = DataAnalyzer(VissimInterface.networks_folder, network_file, raw=False)
+def create_all_ssm(data_source, network_name):
+    if data_source.lower() == 'vissim':
+        data_reader = readers.VissimDataReader(network_name)
+    elif data_source.lower() == 'ngsim':
+        data_reader = readers.NGSIMDataReader(network_name)
+    else:
+        print('Trying to process data from unknown data source')
+        return
+
+    data_analyzer = DataAnalyzer(data_reader.load_data())
 
     print('Computing TTC')
     data_analyzer.include_ttc()
@@ -50,7 +68,7 @@ def create_all_ssm(network_file):
         valid_ttc = df.loc[df['TTC'] < float('inf'), 'TTC']
         print('Mean TTC: {} for {} samples'.format(valid_ttc.mean(), valid_ttc.count()))
     print('Saving TTC')
-    data_analyzer.save_to_csv(VissimInterface.networks_folder)
+    data_analyzer.save_to_csv(network_name)
 
     print('Computing DRAC')
     data_analyzer.include_drac()
@@ -58,14 +76,14 @@ def create_all_ssm(network_file):
         valid_drac = df.loc[df['DRAC'] > 0, 'DRAC']
         print('Mean DRAC: {} for {} samples'.format(valid_drac.mean(), valid_drac.count()))
     print('Saving DRAC')
-    data_analyzer.save_to_csv(VissimInterface.networks_folder)
+    data_analyzer.save_to_csv(network_name)
 
     print('Computing CPI')
     data_analyzer.include_cpi()
     for df in data_analyzer.veh_records.values():
         valid_cpi = df.loc[df['CPI'] > 0, 'CPI']
         print('Mean CPI: {} for {} samples'.format(valid_cpi.mean(), valid_cpi.count()))
-    data_analyzer.save_to_csv(VissimInterface.networks_folder)
+    data_analyzer.save_to_csv(network_name)
     print('Saving CPI')
 
     print('Computing Safe Gaps')
@@ -76,16 +94,18 @@ def create_all_ssm(network_file):
         risky_gaps = df.loc[df['DTSG'] < 0, 'DTSG']
         print('Mean unsafe gap: {} for {} samples'.format(risky_gaps.mean(), risky_gaps.count()))
     print('Saving Safe Gaps')
-    data_analyzer.save_to_csv(VissimInterface.networks_folder)
+    data_analyzer.save_to_csv(network_name)
 
 
 def main():
     # run_i170_scenario(True)
     # network_file = "I710 - MultiSec - 3mi"
     # network_file = 'highway_in_and_out_lanes'
-    # post_process_and_save(network_file)
+    post_process_and_save('ngsim', 'us-101')
     # create_all_ssm(network_file)
-    analyzer = readers.NGSIMDataReader('us-101')
+    # da = DataAnalyzer('vissim', [])
+
+
 
 
 
--- SSMEstimation/readers.py
+++ SSMEstimation/readers.py
@@ -1,12 +1,11 @@
 import os
 import pandas as pd
-from sodapy import Socrata
+# from sodapy import Socrata
 from vissim_interface import VissimInterface
 
 
-# TODO: should this be an abstract class?
 class DataReader:
-    column_names = None
+    relevant_columns = set(['time', 'veh_id', 'veh_type', 'lane', 'x', 'vx', 'y', 'leader_id', 'delta_x', 'delta_v'])
 
     def __init__(self, data_dir, base_file_name):
         self.data_dir = data_dir
@@ -24,18 +23,27 @@ class DataReader:
         Loads csv vehicle records from simulations of a chosen network
         :return: dictionary of dataframes
         """
-        sim_output = dict()
+        # TODO: instead of looking for all files that match, get only one
+        sim_output = []
         for file_name in os.listdir(self.data_dir):
             if file_name.endswith('.csv') and (self.file_name in file_name):
                 file = open(os.path.join(self.data_dir, file_name), 'r')
-                sim_output[os.path.splitext(file_name)[0]] = pd.read_csv(file)
-        return sim_output
+                sim_output.append(pd.read_csv(file))
+        return sim_output.pop()
+
+    def select_relevant_columns(self, data):
+        columns_to_drop = []
+        for col in data.columns:
+            if col not in self.relevant_columns:
+                columns_to_drop.append(col)
+        data.drop(columns=columns_to_drop, inplace=True)
 
 
 class VissimDataReader(DataReader):
 
     def __init__(self, sim_name):
-        self.column_names = ('time', 'veh_id', 'veh_type', 'link', 'lane', 'x', 'vx', 'y', 'leader_id', 'delta_x')
+        self.column_names = ['time', 'veh_id', 'veh_type', 'link', 'lane', 'x', 'vx', 'y',
+                             'leader_id', 'delta_x']
         DataReader.__init__(self, VissimInterface.networks_folder, sim_name)
 
     def load_data(self, sim_number=1):
@@ -52,11 +60,13 @@ class VissimDataReader(DataReader):
                 for line in file:
                     if line.startswith('$VEHICLE'):
                         break
-                sim_output = pd.read_csv(file, sep=';', names=self.column_names)
+                sim_output = pd.read_csv(file, sep=';', names=self.column_names, nrows=1000)
+                print('Still in tests: loading only 1000 first rows')
         except OSError:
             print('{}: File not found'.format(type(self)))
             sim_output = pd.DataFrame()
 
+        self.select_relevant_columns(sim_output)
         return sim_output
 
     def load_max_decel_data(self):
@@ -68,12 +78,15 @@ class VissimDataReader(DataReader):
 
 
 class NGSIMDataReader(DataReader):
-    ngsim_dir = 'C:\\Users\\fvall\\Documents\\Research\\TrafficSimulation\\NGSIM\\'
+    ngsim_dir = 'C:\\Users\\fvall\\Documents\\Research\\TrafficSimulation\\NGSIM_original\\'
     location_switch = {'us-101': 'US-101-LosAngeles-CA\\us-101-vehicle-trajectory-data'}
     interval_switch = {1: '0750am-0805am', 2: '0805am-0820am', 3: '0820am-0835am'}
+    # ['time', 'veh_id', 'veh_type', 'lane', 'x', 'vx', 'y', 'leader_id', 'delta_x', 'delta_v']
+    ngsim_to_reader_naming = {'Global_Time': 'time', 'Vehicle_ID': 'veh_id', 'v_Class': 'veh_type',
+                              'Local_Y': 'x', 'v_Vel': 'vx', 'Local_X': 'y',
+                              'Preceding': 'leader_id', 'Space_Hdwy': 'delta_x'}
 
     def __init__(self, location):
-        self.column_names = ()
         try:
             data_dir = os.path.join(self.ngsim_dir, self.location_switch[location])
             file_name = 'trajectories-'
@@ -84,14 +97,22 @@ class NGSIMDataReader(DataReader):
         DataReader.__init__(self, data_dir, file_name)
 
     def load_data(self, interval=1):
+
+        if interval not in self.location_switch:
+            print('Requested interval not available')
+            return pd.DataFrame()
+
         self.file_name = self.base_file_name + self.interval_switch[interval] + '.csv'
         try:
             with open(os.path.join(self.data_dir, self.file_name), 'r') as file:
-                data = pd.read_csv(file, nrows=100)
+                data = pd.read_csv(file)  # , nrows=1000, skiprows=range(1, 10**4))
+                # print('Still in tests: loading only 100 rows')
+                data.rename(columns=self.ngsim_to_reader_naming, inplace=True)
         except OSError:
             print('{}: File not found'.format(type(self)))
             data = pd.DataFrame()
 
+        self.select_relevant_columns(data)
         return data
 
 
